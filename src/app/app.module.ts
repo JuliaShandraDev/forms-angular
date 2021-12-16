211f2fdfb5d57ae34814fa49e3a51b09cdb73c62
JuliaShandraDev <julia.shandra.dev@gmail.com>
2021-12-16 17:46:35 +0200

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AccordionComponent } from './accordion/accordion.component';
+import { FormBuilderComponent } from './form-builder/form-builder.component';
+import { FieldsComponent } from './fields/fields.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AccordionComponent,
+    FormBuilderComponent,
+    FieldsComponent
   ],
   imports: [
     BrowserModule
